9e18f088ee265ab8c78cb1bb07ccea68e49f871e
Zubola <mkzubola@gmail.com>
2025-02-05 21:51:04 +1300

    add hidden power

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ function roll() {
     document.getElementById("spa").innerHTML = this.getRandomInt(32);
     document.getElementById("spd").innerHTML = this.getRandomInt(32);
     document.getElementById("spe").innerHTML = this.getRandomInt(32);
+    document.getElementById("hiddenpower").innerHTML = this.getHiddenPower(
+        document.getElementById("hp").innerHTML,
+        document.getElementById("atk").innerHTML,
+        document.getElementById("def").innerHTML,
+        document.getElementById("spa").innerHTML,
+        document.getElementById("spd").innerHTML,
+        document.getElementById("spe").innerHTML);
 }
 
 
@@ -22,6 +29,7 @@ function lockin() {
     tr.insertCell().appendChild(document.createTextNode(document.getElementById("spa").innerHTML));
     tr.insertCell().appendChild(document.createTextNode(document.getElementById("spd").innerHTML));
     tr.insertCell().appendChild(document.createTextNode(document.getElementById("spe").innerHTML));
+    tr.insertCell().appendChild(document.createTextNode(document.getElementById("hiddenpower").innerHTML));
     tr.insertCell().appendChild(document.createTextNode(document.getElementById("nature").value));
     tr.insertCell().appendChild(document.createTextNode(document.getElementById("ability").value));
 
@@ -34,4 +42,65 @@ function lockin() {
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+  }
+
+  function getHiddenPower(hp, atk, def, spa, spd, spe) {
+    let type = 0;
+    if (hp % 2 == 1) {
+        type += 1
+    }
+    if (atk % 2 == 1) {
+        type += 2
+    }
+    if (def % 2 == 1) {
+        type += 4
+    }
+    if (spe % 2 == 1) {
+        type += 8
+    }
+    if (spa % 2 == 1) {
+        type += 16
+    }
+    if (spd % 2 == 1) {
+        type += 32
+    }
+    switch (Math.floor(type * 15 / 63)) {
+        case 0: type = "Fighting"; break;
+        case 1: type = "Flying"; break;
+        case 2: type = "Poison"; break;
+        case 3: type = "Ground"; break;
+        case 4: type = "Rock"; break;
+        case 5: type = "Bug"; break;
+        case 6: type = "Ghost"; break;
+        case 7: type = "Steel"; break;
+        case 8: type = "Fire"; break;
+        case 9: type = "Water"; break;
+        case 10: type = "Grass"; break;
+        case 11: type = "Electric"; break;
+        case 12: type = "Psychic"; break;
+        case 13: type = "Ice"; break;
+        case 14: type = "Dragon"; break;
+        case 15: type = "Dark"; break;
+    }
+    let power = 0;
+    if (hp % 4 == 2 || hp % 4 == 3) {
+        power += 1
+    }
+    if (atk % 4 == 2 || atk % 4 == 3) {
+        power += 2
+    }
+    if (def % 4 == 2 || def % 4 == 3) {
+        power += 4
+    }
+    if (spe % 4 == 2 || spe % 4 == 3) {
+        power += 8
+    }
+    if (spa % 4 == 2 || spa % 4 == 3) {
+        power += 16
+    }
+    if (spd % 4 == 2 || spd % 4 == 3) {
+        power += 32
+    }
+    power = Math.floor(power * 40 / 63) + 30
+    return type + " " + power
   }
